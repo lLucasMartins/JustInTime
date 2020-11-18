1e5e2557e7d2eb310e0ed8f00b6f96d4217b40d8
lLucasMartins <lucas.martinsoft@gmail.com>
2020-11-17 22:57:41 -0300

    Services no Cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
-import { AlertController } from '@ionic/angular';
+import { HelperService } from '../services/helper.service';
+/*import { AuthenticationService } from '../services/authentication/authentication.service';*/
+import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -9,42 +13,36 @@ import { AlertController } from '@ionic/angular';
 })
 export class CadastroPage implements OnInit {
 
+  constructor(
+    private helper: HelperService,
+    /*private authentication: AuthenticationService,*/
+    private http: HttpClient) { }
+
+  email = this.getEmail();
   contactField: null;
   messageField: null;
-
-  constructor(
-    private alertController: AlertController
-  ) { }
 
   ngOnInit() {
   }
 
-
-  async sendForm(f: NgForm) {
-
-    const message = 'Contato: ' + this.contactField +
-                    '<br>Mensagem:' + this.messageField;
-
-    const alert = await this.alertController.create({
-      header: 'Alerta!',
-      message,
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Canceled');
-          }
-        }, {
-          text: 'Ok',
-          handler: () => {
-            console.log('Alert Confirmed');
-          }
-        }
-      ]
-    });
-    await alert.present();
+  getEmail() {
+    const email = this.helper.getUrlParameter('email');
+    console.log(email);
+    return email;
   }
 
+  /*sendForm(f: NgForm) {
+    const url = environment.apiUrl + '/saveForm';
+    const dataIn = {
+      contact: this.contactField,
+      message: this.messageField,
+      token: this.authentication.token
+    };
+    this.http.post(url, dataIn)
+      .subscribe(dataOut => {
+        console.log(dataOut);
+      }, error => {
+        console.log(error.message);
+      });
+  }*/
 }
